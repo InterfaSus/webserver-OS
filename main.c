26363c8f7f47c3bb795792631e5005800a99d020
fvaldes0109 <fvaldes0109@gmail.com>
2023-05-24 10:44:51 -0400

    Handle non existent routes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/stat.h>
 
 #include "server.h"
 
@@ -16,7 +17,14 @@ int main(int argc, char *argv[]) {
     
     signal(SIGINT, sigint_handler);
 
-    start_server(port, directory);
+    struct stat st;
+    if (stat(directory, &st) == 0 && S_ISDIR(st.st_mode)) {
+        start_server(port, directory);
+    }
+    else {
+        printf("El directorio especificado no existe\n");
+        return 1;
+    }
 
     return 0;
 }
--- src/routing.c
+++ src/routing.c
@@ -7,6 +7,14 @@
 #include "page.h"
 #include "string_helpers.h"
 
+void send404(char response[]) {
+
+    sprintf(response, "HTTP/1.1 404 Not Found\r\n");
+    sprintf(response + strlen(response), "Content-Type: text/html\r\n");
+    sprintf(response + strlen(response), "\r\n");
+    sprintf(response + strlen(response), "<h1>404 Not Found</h1>");
+}
+
 int router(char *main_directory, char *relative, char response[]) {
 
     char full_dir[MAX_FILE_ROUTE];
@@ -17,7 +25,10 @@ int router(char *main_directory, char *relative, char response[]) {
     struct stat st;
     if (stat(full_dir, &st) == -1) {
         printf("Error al obtener los atributos del archivo %s\n", full_dir);
+        
         // Enviar error 404
+        send404(response);
+
         return 0;
     }
 
@@ -27,7 +38,10 @@ int router(char *main_directory, char *relative, char response[]) {
         FILE *file = fopen(full_dir, "rb");
         if (file == NULL) {
             printf("Error al abrir el archivo %s\n", full_dir);
-            // Enviar error 500 algo
+            
+            // Enviar error 404
+            send404(response);
+
             return 0;
         }
 
